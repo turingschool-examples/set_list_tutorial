ad262e5023a73de7e97e025140616f206b656d97
Brian Zanti <brianzanti@gmail.com>
2021-05-10 16:15:23 -0600

    adds artist songs index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
+  get '/artists/:artist_id/songs', to: 'artist_songs#index'
 end
