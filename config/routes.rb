b3b904d59e64c7d0e5a8b9f7871fee34eae65246
Jamison Ordway <jamisonordway@gmail.com>
2021-09-13 10:28:18 -0600

    Adds material for advanced routing lesson

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/songs', to: 'songs#index'
   get '/songs/:id', to: 'songs#show'
 
   get '/artists/:artist_id/songs', to: 'artist_songs#index'
+
   get '/artists', to: 'artists#index'
+  get '/artists/:id', to: 'artists#show'
   get '/artists/new', to: 'artists#new'
   post '/artists', to: 'artists#create'
   get '/artists/:id/edit', to: 'artists#edit'
   patch '/artists/:id', to: 'artists#update'
   delete '/artists/:id', to: 'artists#destroy'
 
-  get '/playlists', to: "playlists#index"
+  get '/playlists', to: "playlists#index", as: "playlists"
+
+  # resources :artists, only: [:show, :update, :destroy]
+  
+  # namespace :artists do
+  #   resources :songs
+  # end
+
+  # scope :artists do
+  #   resources :songs
+  # end
+
+  # scope module: :artists do
+  #   resources :songs
+  # end
 end
