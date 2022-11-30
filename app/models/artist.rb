e853d668e6f418a9e0243d12452c0b996ba70d07
Brian Zanti <brianzanti@gmail.com>
2022-11-29 18:28:17 -0600

    adds solutions

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,4 +4,23 @@ class Artist < ApplicationRecord
   def average_song_length
     self.songs.average(:length)
   end
+
+# describe '#songs' do
+  # def songs
+  #   Song.where(artist_id: self.id)
+  # end
+
+# describe '#song_count'
+  def song_count
+    self.songs.count
+  end
+
+# describe '#wrote_song?' do
+  def wrote_song?(song)
+    # Version 1 WITH Assocations
+    self.songs.include?(song)
+
+    # Version 2 WITHOUT Associations
+    # song.artist_id == self.id
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,18 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+# describe '#artist' do
+#   def artist
+#     Artist.find(self.artist_id)
+#   end
+
+# describe '#artist_name' do
+  def artist_name
+    self.artist.name
+  end
+
+# describe '#other_artist_songs' do
+  def other_artist_songs
+    self.artist.songs.where.not(id: self.id)
+  end
 end
