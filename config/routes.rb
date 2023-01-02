ae22d740550bea057b2b9e43ab4bbf35ebaaf489
Chris Simmons <chris@turing.edu>
2023-01-02 11:52:31 -0500

    Refactors routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,25 @@
 Rails.application.routes.draw do
-  get '/songs', to: 'songs#index'
-  get '/songs/:id', to: 'songs#show'
+  # get '/songs', to: 'songs#index'
+  # get '/songs/:id', to: 'songs#show'
 
-  get '/artists/:artist_id/songs', to: 'artist_songs#index'
+  # get '/artists/:artist_id/songs', to: 'artist_songs#index'
 
-  get '/artists', to: 'artists#index'
-  get '/artists/:id', to: 'artists#show'
-  get '/artists/new', to: 'artists#new'
-  post '/artists', to: 'artists#create'
-  get '/artists/:id/edit', to: 'artists#edit'
-  patch '/artists/:id', to: 'artists#update'
-  delete '/artists/:id', to: 'artists#destroy'
+  # get '/artists', to: 'artists#index'
+  # get '/artists/:id', to: 'artists#show'
+  # get '/artists/new', to: 'artists#new'
+  # post '/artists', to: 'artists#create'
+  # get '/artists/:id/edit', to: 'artists#edit'
+  # patch '/artists/:id', to: 'artists#update'
+  # delete '/artists/:id', to: 'artists#destroy'
 
-  get '/playlists', to: "playlists#index", as: "playlists"
+  # get '/playlists', to: "playlists#index", as: "playlists"
 
-  # resources :artists, only: [:show, :update, :destroy]
-  
-  # namespace :artists do
-  #   resources :songs
-  # end
+  resources :artists do
+    resources :songs, only: [:index], controller: "artist_songs"
+  end
 
-  # scope :artists do
-  #   resources :songs
-  # end
+  resources :songs, only: [:index, :show]
 
-  # scope module: :artists do
-  #   resources :songs
-  # end
+  resources :playlists, only: [:index]
+
 end
