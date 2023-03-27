0fb24ed964e43a66a2d91d81f19fd8c342861067
Chris Simmons <chris@turing.edu>
2023-03-27 11:58:28 -0400

    Creates homework exercise and test data

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -2,4 +2,48 @@ require 'rails_helper'
 
 RSpec.describe Song, type: :model do
   it {should belong_to :artist}
+
+  # HOMEWORK SETUP INSTRUCTIONS
+  # First, make sure your bundle & database are up to date
+    # bundle install
+    # rails db:{drop,create,migrate,seed}
+
+  # Your task is to write AR queries and set them to the `query` variable in each test. When you run each test, your query should make the test pass. 
+
+  describe "class methods" do
+    before :each do 
+      test_data # This method just loads test data from spec_helper; it is the same as the seeds data. Do not change the values of the objects to make your tests pass! 
+    end
+
+
+    it "Get all songs" do
+      query = #write your query here!
+      
+      expect(query.sort).to eq([@purple_rain, @beret, @legend, @talk, @twenty_six, @vagabond, @bury_a_friend, @bad_guy, @someone_great, @change].sort)
+    end
+
+    it "Get the lengths of all songs" do
+      query = #write your query here!
+
+      expect(query).to eq([845, 665, 2301, 2301, 940, 240, 340, 240, 500, 640])
+    end
+
+    it "Get the songs with a play count greater than 0" do
+      query = #write your query here!
+
+      expect(query.sort).to eq([@purple_rain, @beret, @legend, @talk, @twenty_six, @vagabond, @bury_a_friend, @bad_guy, @someone_great, @change].sort)    
+    end
+
+    it "Get the titles of the songs with a play count greater than zero, sorted alphabetically" do
+      query = # write your query here!
+
+      expect(query.sort).to eq(["26", "I Can Change", "Legend Has It", "Purple Rain", "Raspberry Beret", "Someone Great", "Talk to Me", "Vagabond", "bad guy", "bury a friend"])
+    end
+
+    it "Get the length of the song with the most plays" do
+      query = # write your query here!
+      
+      expect(query).to eq(2301)
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,48 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
+
+def test_data
+  @prince = Artist.create!(name: 'Prince')
+  @rtj = Artist.create!(name: 'Run The Jewels')
+  @caamp = Artist.create!(name: 'Caamp')
+  @jgb = Artist.create!(name: 'Jerry Garcia Band')
+  @billie = Artist.create!(name: 'Billie Eilish')
+  @lcd = Artist.create!(name: 'LCD Soundsystem')
+
+  @purple_rain = @prince.songs.create!(title: 'Purple Rain', length: 845, play_count: 8599)
+  @beret = @prince.songs.create!(title: 'Raspberry Beret', length: 665, play_count: 99)
+
+  @legend = @rtj.songs.create!(title: 'Legend Has It', length: 2301, play_count: 2300000)
+  @talk = @rtj.songs.create!(title: 'Talk to Me', length: 2301, play_count: 2300000)
+
+  @twenty_six = @caamp.songs.create!(title: '26', length: 940, play_count: 150000)
+  @vagabond = @caamp.songs.create!(title: 'Vagabond', length: 240, play_count: 120000)
+
+  @bury_a_friend = @billie.songs.create!(title: 'bury a friend', length: 340, play_count: 1200000)
+  @bad_guy = @billie.songs.create!(title: 'bad guy', length: 240, play_count: 100000)
+
+  @someone_great = @lcd.songs.create!(title: 'Someone Great', length: 500, play_count: 1000000)
+  @change = @lcd.songs.create!(title: 'I Can Change', length: 640, play_count: 100000)
+
+  @summer_rewind = Playlist.create!(name: 'summer rewind')
+  @workout = Playlist.create!(name: 'workout')
+  @chill_evening = Playlist.create!(name: 'chill evening')
+
+  PlaylistSong.create!(playlist: @summer_rewind, song: @beret)
+  PlaylistSong.create!(playlist: @summer_rewind, song: @bad_guy)
+
+  PlaylistSong.create!(playlist: @chill_evening, song: @purple_rain)
+  PlaylistSong.create!(playlist: @chill_evening, song: @change)
+  PlaylistSong.create!(playlist: @chill_evening, song: @talk)
+
+  @s26 = Song.new(title: "26")
+  @sican = Song.new(title: "I Can Change")
+
+
+end
+
+
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
