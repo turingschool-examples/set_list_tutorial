1cbb0e9e34a852d27ab028b8440d9fca08daa3fd
Chris Simmons <chris@turing.edu>
2023-03-27 12:18:54 -0400

    Adds join problem solution

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -4,4 +4,17 @@ RSpec.describe Playlist, type: :model do
   describe "relationships" do
     it { should have_many :playlist_songs}
   end
+
+  describe "class methods" do
+    before :each do
+      test_data # check the `spec_helper` file to see which songs are in the Summer Rewind Playlist. 
+    end
+    it "Get the names of all Artists from the Summer Rewind Playlist. " do
+      # Hint: Use the @summer_rewind playlist for this query. 
+
+      query = Artist.joins(songs: :playlists).where("playlist_id = #{@summer_rewind.id}").pluck("artists.name")
+
+      expect(query.sort).to eq(["Prince", "Billie Eilish"])
+    end
+  end
 end
